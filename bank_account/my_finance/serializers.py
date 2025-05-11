f6962959319b397729583ff200e1fcb0acf2e4ea
murmeowcode <grisha23.01.2004@gmail.com>
2025-05-11 12:46:09 +0300

    upd permissions

--- bank_account/my_finance/serializers.py
+++ bank_account/my_finance/serializers.py
@@ -1,9 +1,28 @@
-from rest_framework.serializers import Serializer, ModelSerializer
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import  User
 
         
 class UserSerializer(ModelSerializer):
-    
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['phone','username','first_name','last_name','email']
+        fields = ['phone', 'username', 'first_name', 'last_name', 'email', 'password']
+
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # хеширование пароля
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)  # хеширование пароля при обновлении
+        instance.save()
+        return instance
--- bank_account/my_finance/views.py
+++ bank_account/my_finance/views.py
@@ -3,8 +3,9 @@ from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import  UserSerializer
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from permissions import IsAdminOrOwner
 
 class UserViewSet(ModelViewSet):
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminOrOwner]
     queryset = User.objects.all()
     serializer_class = UserSerializer
